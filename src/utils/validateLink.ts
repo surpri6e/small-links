71873ffa857d657ba3725a94d618bec201b53353
surpri6e <dan.surpri6e@gmail.com>
2024-05-07 21:09:43 +0300

    new validate link

--- src/utils/validateLink.ts
+++ src/utils/validateLink.ts
@@ -1,6 +1,6 @@
 /** Check link it or no */
 export function validateLink(str: string): boolean {
-   const regex = /^(https?:\/\/)?([\da-z\\.-]+)\.([a-z\\.]{2,6})([\\/\w \\.-]*)*\/?$/;
+   const regex = /^(https?:\/\/)/;
 
    if (regex.test(str)) {
       return true;
